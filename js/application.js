e328f88fee8099cf8137a91fe69e4bc8d95b7694
Zhu Wu <zhu.wu@outlook.com>
2014-09-02 14:30:59 +0800

    Add random number generator & random position generator.

--- js/application.js
+++ js/application.js
@@ -30,9 +30,83 @@ angular.module('game2048', [])
       }
     };
   }])
-  .factory('Transformer', [function(){
+  .factory('Validator', [function() {
     return {
-      generateRandom: function(data, numberOfEmptySlot) {
+      countEmptySlot: function(data) {
+        var emptySlot = 0;
+        for (var i = 0; i < data.length; i++) {
+          for (var j = 0; j < data[i].length; j++) {
+            if (data[i][j] === 0) {
+              emptySlot++;
+            }
+          }
+        }
+        return emptySlot;
+      },
+      
+      checkAvailableMove: function(data) {
+        
+      }
+    }
+  }])
+  .factory('RandomGenerator', [function() {
+    return {
+      generateRandomPositions: function(upperLimit, numberOfPositions) {
+        if (numberOfPositions > upperLimit || upperLimit <= 0 || numberOfPositions <= 0) {
+          throw "Error: data error.";
+        }
+        
+        if (numberOfPositions == 1) {
+          return [Math.floor(Math.random() * upperLimit)];
+        } else {
+          return [3, 4];
+        }
+      },
+      
+      generateOneRandomPosition: function(upperLimit) {
+        return ;
+      },
+      
+      generateTwoRandomPosition: function(upperLimit) {
+        
+      },
+      
+      pickRandomValue: function() {
+        return Math.random() * 10 < 9 ? 2 : 4;
+      }
+    }
+  }])
+  .factory('Transformer', ['Validator', 'RandomGenerator', function(Validator, RandomGenerator) {
+    return {
+      fillInSlots: function(data, numberOfSlotsToBeFilledIn) {
+        var emptySlot = Validator.countEmptySlot(data);
+        
+        if (emptySlot >= numberOfSlotsToBeFilledIn) {
+          var randomPositions = RandomGenerator.generateRandomPositions(emptySlot, numberOfSlotsToBeFilledIn),
+              emptyCounter = 0,
+              updatedCounter = 0;
+          
+          for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[i].length; j++) {
+              if (emptyCounter == randomPositions[updatedCounter]) {
+                data[i][j] = RandomGenerator.pickRandomValue();
+                updatedCounter++;
+                emptyCounter++;
+                if (updatedCounter >= numberOfSlotsToBeFilledIn) {
+                  break;
+                }
+              }
+              
+              if (data[i][j] === 0) {
+                emptyCounter++;
+              }
+            }
+            if (updatedCounter >= numberOfSlotsToBeFilledIn) {
+              break;
+            }
+          }
+        }
+        
         return data;
       },
       
@@ -41,9 +115,8 @@ angular.module('game2048', [])
       }
     };
   }])
-  .controller('GameController', ["$scope", 'Transformer', function($scope, Transformer) {
-    $scope.data = Transformer.generateRandom(
-            Transformer.generateRandom([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 16), 15);
+  .controller('GameController', ['$scope', 'Transformer', function($scope, Transformer) {
+    $scope.data = Transformer.fillInSlots([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 1);
     $scope.$on('move', function(e, direction) {
       console.log(direction);
     });    
